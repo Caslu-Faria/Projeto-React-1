5b99970125c7f485491692398bd7d78bca2a184a
Caslu-Faria <lucasglf.contato@gmail.com>
2022-07-09 13:06:02 -0300

    Projeto react 1 v3, respoisividade

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import Avatar from '../Avatar/Avatar';
 import styles from './Comment.module.css'
 
 function Comment(){
     return(
         <div className={styles.comment}>
-            <img src="https://pps.whatsapp.net/v/t61.24694-24/244742221_489976699420715_4215383149013828570_n.jpg?ccb=11-4&oh=b13a2216e4208f74e9d3f6212e88c421&oe=62D6F2C2" />
+            <Avatar hasBorder={false} src="https://pps.whatsapp.net/v/t61.24694-24/244742221_489976699420715_4215383149013828570_n.jpg?ccb=11-4&oh=b13a2216e4208f74e9d3f6212e88c421&oe=62D6F2C2" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
@@ -14,14 +15,15 @@ function Comment(){
                             <time title="8 de julho de 2022 ás 17:32" dateTime="2022-07-08 17:32:52">Cerca de 1h atrás</time>
                         </div>
                     
-
-                    <p>Muito incrivel esse teste</p>
-              
-                
                         <button title="Deletar comentário">
                             <Trash size={24} />
                         </button>
-                    </header>  
+                    </header>
+                    
+
+                    <p>Muito incrivel esse teste</p>
+              
+                  
                 </div>
 
                 <footer>
--- src/components/Posts/Index.jsx
+++ src/components/Posts/Index.jsx
@@ -1,3 +1,4 @@
+import Avatar from '../Avatar/Avatar'
 import Comment from '../Comment/Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src= "https://pps.whatsapp.net/v/t61.24694-24/244742221_489976699420715_4215383149013828570_n.jpg?ccb=11-4&oh=b13a2216e4208f74e9d3f6212e88c421&oe=62D6F2C2" />
+                    <Avatar hasBorder={true} src="https://pps.whatsapp.net/v/t61.24694-24/244742221_489976699420715_4215383149013828570_n.jpg?ccb=11-4&oh=b13a2216e4208f74e9d3f6212e88c421&oe=62D6F2C2" />
                     <div className={styles.authorInfo}>
                         <strong>Lucas Faria</strong>
                         <span>Web Dev at PyxisWeb</span>
